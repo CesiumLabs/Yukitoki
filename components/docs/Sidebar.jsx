d5e05ac35efc58b26846c07c9a2e2c8d36a82894
Snowflake107 <devsnowflake107@gmail.com>
2021-04-22 22:02:04 +0545

    some sorta progress

--- components/docs/Sidebar.jsx
+++ components/docs/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Converter } from "showdown";
+import Viewer from "./Viewer";
 import hljs from "highlight.js";
 import 'highlight.js/styles/monokai-sublime.css';
 
@@ -14,15 +15,16 @@ const mdconv = new Converter({
 const makeHTML = (md) => mdconv.makeHtml(md);
 
 export default function Sidebar({ docs }) {
-    const [docsContent, setDocsContent] = useState();
+    const [docsContent, setDocsContent] = useState({ jsx: false, data: null });
     const data = Object.keys(docs).filter(a => params.includes(a)).map(m => ({ name: m, data: docs[m] }));
     const custom = Object.values(docs.custom);
-    if (!docsContent) {
+    
+    if (!docsContent.data) {
         let doccont = Object.values(custom[0].files)[0];
 
         if (doccont.type === "js") doccont.content = `\`\`\`js\n${doccont.content}\`\`\``;
 
-        setDocsContent(makeHTML(doccont.content));
+        setDocsContent({ jsx: false, data: makeHTML(doccont.content) });
     }
 
     useEffect(() => highlight());
@@ -31,14 +33,19 @@ export default function Sidebar({ docs }) {
         e.preventDefault();
     }
 
-    function navigate(navigateTo, e) {
+    function navigate(navigateTo, e, notCustom = false) {
         e.preventDefault();
-        const loc = custom.find(x => x.files[navigateTo] !== undefined);
-        const file = loc.files[navigateTo];
 
-        if (file.type === "js") file.content = `\`\`\`js\n${file.content}\`\`\``;
+        if (!notCustom) {
+            const loc = custom.find(x => x.files[navigateTo] !== undefined);
+            const file = loc.files[navigateTo];
 
-        setDocsContent(makeHTML(file.content))
+            if (file.type === "js") file.content = `\`\`\`js\n${file.content}\`\`\``;
+
+            setDocsContent({ jsx: false, data: makeHTML(file.content) });
+        } else {
+            setDocsContent({ jsx: true, data: notCustom });
+        }
     }
 
     function highlight() {
@@ -109,6 +116,7 @@ export default function Sidebar({ docs }) {
                                                         return (
                                                             <li key={i + j}>
                                                                 <a href="#"
+                                                                    onClick={(e) => navigate(m.data[n].name, e, m.data[n])}
                                                                     className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-700 text-gray-500 hover:text-gray-200 border-l-4 border-transparent hover:border-blue-500 pr-6">
                                                                     <span className="inline-flex justify-center items-center ml-4">
                                                                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
@@ -129,7 +137,9 @@ export default function Sidebar({ docs }) {
                 </div>
 
                 <div className="inline-grid docs-container px-5 lg:border-l-2 lg:border-gray-300">
-                    <div id="docs-content" className="text-white prose" dangerouslySetInnerHTML={{ __html: docsContent }}></div>
+                    <div id="docs-content" className="text-white prose">
+                        <Viewer data={docsContent} />
+                    </div>
                 </div>
             </div>
         </div>
