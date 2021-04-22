98229dfb5863e374635ce70019a2c46b3c26fbcc
Snowflake107 <devsnowflake107@gmail.com>
2021-04-22 19:49:38 +0545

    todo

--- components/Home.jsx
+++ components/Home.jsx
@@ -38,7 +38,7 @@ export default class Home extends Component {
                                 <code className="shadow-xl text-white bg-gray-600 font-bold leading-none p-3 rounded-md">
                                     <h1 className="text-lg">{INSTALL_COMMAND}</h1>
                                 </code>
-                                <FontAwesomeIcon icon={faClipboard} className="w-10 h-10 text-white hover:text-gray-300 my-1 cursor-pointer" onClick={() => navigator.clipboard.writeText(INSTALL_COMMAND).catch(e => {})} />
+                                {/* <FontAwesomeIcon icon={faClipboard} className="w-10 h-10 text-white hover:text-gray-300 my-1 cursor-pointer" onClick={() => navigator.clipboard.writeText(INSTALL_COMMAND).catch(e => {})} /> */}
                             </div>
 
                             <div className="buttons flex space-x-2 justify-center align-center mb-10">
