dce9f8322f1c0f7b044424557aa798afc3fbcab7
Snowflake107 <devsnowflake107@gmail.com>
2021-04-24 16:57:26 +0545

    metadata

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,10 +9,22 @@ export default function Application({ Component, pageProps }) {
             <Head>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+
                 <meta property="og:url" content={META.URL} />
                 <meta property="og:title" content={META.TITLE} />
                 <meta property="og:description" content={META.DESCRIPTION} />
                 <meta property="og:image" content={META.IMAGE} />
+
+                <meta property="url" content={META.URL} />
+                <meta property="title" content={META.TITLE} />
+                <meta property="description" content={META.DESCRIPTION} />
+                <meta property="image" content={META.IMAGE} />
+
+                <meta property="twitter:url" content={META.URL} />
+                <meta property="twitter:title" content={META.TITLE} />
+                <meta property="twitter:description" content={META.DESCRIPTION} />
+                <meta property="twitter:image" content={META.IMAGE} />
+
                 <meta name="theme-color" content="#4285F4" />
                 <link rel="shortcut icon" href="/favicon.png" />
                 <title>{META.TITLE}</title>
