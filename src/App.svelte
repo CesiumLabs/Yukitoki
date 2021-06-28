2450a9db73161f8789e8e1eac5f1fbaa7ac2a3f1
Snowflake107 <devsnowflake107@gmail.com>
2021-06-28 11:12:50 +0545

    remove unnecessary style

--- src/App.svelte
+++ src/App.svelte
@@ -23,24 +23,14 @@
     @tailwind components;
     @tailwind utilities;
 
-    /* width */
     ::-webkit-scrollbar {
         width: 5px;
         height: 5px;
     }
-
-    /* Track */
     ::-webkit-scrollbar-track {
         background: #ffffff;
     }
-
-    /* Handle */
     ::-webkit-scrollbar-thumb {
-        background: #304eb6;
-    }
-
-    /* Handle on hover */
-    ::-webkit-scrollbar-thumb:hover {
         background: #304eb6;
     }
 </style>
