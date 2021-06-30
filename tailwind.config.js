3c9b1dd67e18d6d873ef6f9af5c3fcc054a4f9f8
Snowflake107 <devsnowflake107@gmail.com>
2021-06-30 12:22:04 +0545

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-    purge: {
-        enabled: !process.env.ROLLUP_WATCH,
-        content: ["./public/index.html", "./src/**/*.svelte"],
-        options: {
-            defaultExtractor: (content) => [...(content.match(/[^<>""`\s]*[^<>""`\s:]/g) || []), ...(content.match(/(?<=class:)[^=>\/\s]*/g) || [])]
-        }
-    },
+    purge: ["./public/index.html", "./src/**/*.svelte"],
     darkMode: "class",
     theme: {
         extend: {
