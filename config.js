83c56d1c386f957d697830fabb7f162471f1bd81
Snowflake107 <devsnowflake107@gmail.com>
2021-07-01 19:20:00 +0545

    move doc links to config

--- config.js
+++ config.js
@@ -38,11 +38,37 @@ const config = {
     },
     DESCRIPTION: {
         TITLE: "About",
-        VALUE: "A customizable website to view docs for Snowflake Studio ❄'s projects"
+        VALUE: "A customizable website to view docs for your projects"
     },
     FOOTER: {
         HEADER: "Yukitoki",
         DESCRIPTION: "Custom documentation viewing website"
+    },
+    LINKS: {
+        string: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String",
+        number: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number",
+        bigint: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt",
+        boolean: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean",
+        symbol: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol",
+        void: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/undefined",
+        Object: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
+        Function: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
+        function: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
+        Array: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
+        Set: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set",
+        Map: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map",
+        Date: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date",
+        RegExp: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp",
+        Promise: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise",
+        Error: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error",
+        EventEmitter: "https://nodejs.org/dist/latest/docs/api/events.html#events_class_eventemitter",
+        Timeout: "https://nodejs.org/dist/latest/docs/api/timers.html#timers_class_timeout",
+        Immediate: "https://nodejs.org/dist/latest/docs/api/timers.html#timers_class_immediate",
+        Buffer: "https://nodejs.org/dist/latest/docs/api/buffer.html#buffer_class_buffer",
+        ReadableStream: "https://nodejs.org/dist/latest/docs/api/stream.html#stream_class_stream_readable",
+        ChildProcess: "https://nodejs.org/dist/latest/docs/api/child_process.html#child_process_class_childprocess",
+        Worker: "https://nodejs.org/api/worker_threads.html#worker_threads_class_worker",
+        MessagePort: "https://nodejs.org/api/worker_threads.html#worker_threads_class_messageport"
     }
 };
 
