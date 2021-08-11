c0e222ccb69b89eabc703a2064ad4b45cb5265bd
Andromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-11 09:21:24 +0545

    fix(SoundCloudSource): allow master branch

--- src/data/SoundCloudSource.js
+++ src/data/SoundCloudSource.js
@@ -1,14 +1,14 @@
 import DocsManager from "./DocsManager";
 import semver from "semver";
 
-const blacklisted = new Set(["docs", "master"]);
+const blacklisted = new Set(["docs"]);
 
 export default new DocsManager({
     id: "soundcloud",
     name: "Main",
     global: "SoundCloud",
     repo: "DevSnowflake/soundcloud-scraper",
-    defaultTag: "rewrite",
+    defaultTag: "master",
     branchFilter: (branch) => !blacklisted.has(branch) && !branch.startsWith("dependabot/"),
     tagFilter: (tag) => semver.gte(tag.replace("v", ""), "4.0.0")
 });
