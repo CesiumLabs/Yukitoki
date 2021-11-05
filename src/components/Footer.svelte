f35e1200462c606883e9c7eb7edfe8b6da18ad51
and <46562212+DevAndromeda@users.noreply.github.com>
2021-11-05 20:30:57 +0545

    feat: deploy to netlify button

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,6 +7,10 @@
     <p class="text-lg text-white mb-3">{config.FOOTER.DESCRIPTION}</p>
 
     <hr class="w-1/3 mx-auto border-t-2 border-gray-200 py-3" />
+    
+    <a href="https://app.netlify.com/start/deploy?repository=https://github.com/DevSnowflake/Yukitoki">
+        <img class="mx-auto" src="https://www.netlify.com/img/deploy/button.svg"/>
+    </a>
 
     <p class="text-white">Yukitoki - &copy; <a class="hover:text-gray-300" href="https://github.com/DevSnowflake">Snowflake Studio ❄</a> - {new Date().getFullYear()}</p>
 </footer>
